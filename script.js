f14f42ddf0f68ca8eb3e5fc7c2299f8bb8218569
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-27 13:40:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const addTheWordCool = function(array){
+    // add your code
+    return addTheWordCool.push = ["cool"];
+}
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+// resultaat: ["nice", "awesome", "tof", "cool"]
